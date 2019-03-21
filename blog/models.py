50cccd37d5db11ae3bf927e3c51021f582259ab0
omar ayman <omaraymanomar@gmail.com>
2019-03-22 00:43:17 +0200

    signup

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User',
@@ -8,4 +8,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-        
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Login
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
